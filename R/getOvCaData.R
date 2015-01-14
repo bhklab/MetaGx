77b6dcb3c556a16555d5bafb6b9ea5dbf3e8ddcc
natchar <Natchar.r@alumni.ubc.ca>
2015-01-14 10:06:17 -0500

    Update getOvCaData.R

--- R/getOvCaData.R
+++ R/getOvCaData.R
@@ -1,12 +1,15 @@
 ########################
 ## Benjamin Haibe-Kains
 ## All rights Reserved
-## December 23, 2013
+## January 14, 2015
+## Initial modification of getBrCaData for Ovarian (OvarianCuratedDataset) by  Deena and Natchar
 ########################
 
 `getOvCaData` <- 
 function (resdir="cache", probegene.method, remove.duplicates=TRUE, topvar.genes=1000, duplicates.cor=0.98, datasets, sbt.model=c("scmgene", "scmod2", "scmod1", "pam50", "ssp2006", "ssp2003"), merging.method=c("union", "intersection"), merging.std=c("quantile", "robust.scaling", "scaling", "none"), nthread=1, verbose=TRUE) {  
 
+#Need to remove InSilicoDB reference
+# Keep Normalization, merging?
   stop("getOvCaData is not implemented yet")
   
 }
