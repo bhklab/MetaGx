8bab45992d5e9be8839e0716d3d0fd283c4fd89f
natchar <Natchar.r@alumni.ubc.ca>
2015-01-15 17:34:38 -0500

    Update getOvCaData.R

--- R/getOvCaData.R
+++ R/getOvCaData.R
@@ -11,7 +11,48 @@ function (resdir="cache", probegene.method, remove.duplicates=TRUE, topvar.genes
 #Need to remove InSilicoDB reference
 # Keep Normalization, merging?
   stop("getOvCaData is not implemented yet")
-  
+
+
+
+
+#################
+## loading and changing curatedOvarianData
+## Natchar January 15, 2015
+#################
+
+# Load the curatedOvarianData package
+library(curatedOvarianData)
+
+# Load the filtering rules from patientselection.config
+source(system.file("extdata", "patientselection.config", package = "curatedOvarianData"))
+
+# Load logging package
+library(logging)
+
+# Get rid of duplicates and load esets into the environment
+source(system.file("extdata", "createEsetList.R", package = "curatedOvarianData"))
+
+#columns of pdata we want
+PHENOdata <- pData(esets[[1]]) [,c("sample_type", "grade", "age_at_initial_pathologic_diagnosis", "days_to_tumor_recurrence", "days_to_death", "os_binary", "relapse_binary", "batch")]
+
+# assayData matrix
+exprs <- matrix(0, nrow(fData(esets[[1]])), ncol = nrow(pData(esets[[1]])))
+
+# phenodata in AnnotatedDataFrameß
+PData <- AnnotatedDataFrame(data = PHENOdata)
+FData <- AnnotatedDataFrame(data = fData(esets[[1]]))
+
+# Rename pData to standard names
+colnames(PData) <- c("tissue", "grade", "age", "t.trs", "t.os", "e.os", "e.rfs", "series")
+
+# make the expression set with exprs and PData
+neweset<- ExpressionSet(exprs, phenoData =PData, experimentData=experimentData(esets[[1]]), featureData = featureData(esets[[1]]),  protocolData= protocolData(esets[[1]]), annotation=annotation(esets[[1]]))
+
+
+
+
+
+
 }
 
 #####
