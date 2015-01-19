98247d04a06d3d6d324648412e54efcd002ad6b3
natchar <Natchar.r@alumni.ubc.ca>
2015-01-19 11:02:56 -0500

    Update getOvCaData.R
    OvarianEsests now contains all the ovarian expression sets reformatted to be similar to the breast cancer expression sets

--- R/getOvCaData.R
+++ R/getOvCaData.R
@@ -10,13 +10,10 @@ function (resdir="cache", probegene.method, remove.duplicates=TRUE, topvar.genes
 
 #Need to remove InSilicoDB reference
 # Keep Normalization, merging?
-  stop("getOvCaData is not implemented yet")
-
-
 
 #################
 ## loading and changing curatedOvarianData
-## Natchar January 16, 2015
+## Natchar January 19, 2015
 #################
 
 # Load the curatedOvarianData package
@@ -32,112 +29,125 @@ library(logging)
 source(system.file("extdata", "createEsetList.R", package = "curatedOvarianData"))
 
 
-################# columns of pdata we want
-PHENOdata <- pData(esets[[1]]) [,c("sample_type", "grade", "age_at_initial_pathologic_diagnosis", "days_to_tumor_recurrence", "days_to_death", "os_binary", "relapse_binary", "batch")]
-
-
-################# change sample_type: all "healthy" to "normal"
-# initialize lists
-
-oldList <- PHENOdata[,"sample_type"]
-listIndex <- which(oldList == "healthy")
-# listIndex <- NULL
-# for(i in 1:length(oldList)) {
-#   if (oldList[i] == "healthy") {listIndex <- c(listIndex, i)}
-# }
-
-newList <- replace(oldList, listIndex, "normal")
-
-# replacing the old sample_type column with the new column
-PHENOdata[,"sample_type"] <- newList
+OvarianEsets <- list()
+for(i in 1:length(esets)){
+  currenteset <- esets[[i]]
+  ################# columns of pdata we want
+  PHENOdata <- pData(esets[[i]]) [,c("alt_sample_name", "sample_type", "grade", "age_at_initial_pathologic_diagnosis", "days_to_tumor_recurrence", "days_to_death", "os_binary", "relapse_binary", "batch")]
   
   
-################# add dataset column 
-dataset <- matrix(names(esets)[1], nrow= nrow(pData(esets[[1]])), ncol=1)
-PHENOdata <- cbind(PHENOdata, dataset)
-
-
-
-################# add treatment column made from pltx/tax/neo
-# # treatment column will be a list of tables 
-# 
-# pltx <- pData(esets[[1]])[,"pltx"]
-# tax <- pData(esets[[1]])[,"tax"]
-# neo <- pData(esets[[1]])[,"neo"]
-# 
-# treatment_values <- list(pltx, tax, neo)
-# names(treatment_values) <- c("pltx", "tax", "neo")
-
-# treatments <- list()
-# for (i in 1:length(pltx)){
-#   treatments[i]<- data.frame(treatment_values[[1]][i], treatment_values[[2]][i], treatment_values [[3]][i])
-# }
-
-# 
-# treatment_table <- cbind(pltx, tax, neo)
-# treatment_values <- NULL
-# for(i in 1:length(pltx)){
-#   treatment_values <- list(treatment_values, treatment_table[i,])
-# }
-
-
-################# add treatment column made from pltx/tax/neo
-# save columns as a table lists of "y", "n" or NA, if mix of n and NA, produce n
-
-pltx <- pData(esets[[1]])[,"pltx"]
-tax <- pData(esets[[1]])[,"tax"]
-neo <- pData(esets[[1]])[,"neo"]
-
-treatment_table <- cbind(pltx, tax, neo)
-row <- NULL
-treatment_values <- NULL
-single_value <- NULL
-yindex <-NULL
-for(i in 1:length(pltx)){
-  row <- treatment_table[i,]
-  # if they are all NA, put NA, if all n, put n, if all y, then list all of them
-  if ((all(row == row[1])) == TRUE || all(lapply(row, is.na) == TRUE)){
-    if (is.na(row[1]) || row[1] == "n"){ #all NA, all "n"
-      single_value <- row[1]
-    } else {# all are y!
-      single_value <- paste(as.character(colnames(treatment_table)), collapse = "/")
-    }
+  ################# change sample_type: all "healthy" to "normal"
+  # initialize lists
+  
+  oldList <- PHENOdata[,"sample_type"]
+  listIndex <- which(oldList == "healthy")
+  # listIndex <- NULL
+  # for(i in 1:length(oldList)) {
+  #   if (oldList[i] == "healthy") {listIndex <- c(listIndex, i)}
+  # }
+  
+  newList <- replace(oldList, listIndex, "normal")
+  
+  # replacing the old sample_type column with the new column
+  PHENOdata[,"sample_type"] <- newList
+  
+  
+  ################# add dataset column 
+  dataset <- matrix(names(esets)[i], nrow= nrow(pData(esets[[i]])), ncol=1)
+  PHENOdata <- cbind(PHENOdata, dataset)
+  
+  
+  
+  ################# add treatment column made from pltx/tax/neo
+  # # treatment column will be a list of tables 
+  # 
+  # pltx <- pData(currenteset)[,"pltx"]
+  # tax <- pData(currenteset)[,"tax"]
+  # neo <- pData(currenteset)[,"neo"]
+  # 
+  # treatment_values <- list(pltx, tax, neo)
+  # names(treatment_values) <- c("pltx", "tax", "neo")
+  
+  # treatments <- list()
+  # for (i in 1:length(pltx)){
+  #   treatments[i]<- data.frame(treatment_values[[1]][i], treatment_values[[2]][i], treatment_values [[3]][i])
+  # }
+  
+  # 
+  # treatment_table <- cbind(pltx, tax, neo)
+  # treatment_values <- NULL
+  # for(i in 1:length(pltx)){
+  #   treatment_values <- list(treatment_values, treatment_table[i,])
+  # }
+  
+  
+  ################# add treatment column made from pltx/tax/neo
+  # save columns as a table lists of "y", "n" or NA, if mix of n and NA, produce n
+  
+  pltx <- pData(esets[[i]])[,"pltx"]
+  tax <- pData(esets[[i]])[,"tax"]
+  neo <- pData(esets[[i]])[,"neo"]
+  
+  treatment_table <- cbind(pltx, tax, neo)
+  row <- NULL
+  treatment_values <- NULL
+  single_value <- NULL
+  naindex <-NULL
+  yindex <-NULL
+  IndexedRow <- NULL
+  
+  for(k in 1:length(pltx)){
+    row <- treatment_table[k,]
+    # map NA values to "na" to avoid missing value errors
+    naindex <- which(is.na(row))
+    IndexedRow <- replace(row, naindex, "na")
     
-  ### if entries are not all the same, take y's  
-  } else {
-    #refresh single_value
-    single_value <- NULL 
-    # if none of the elements are y, then mix of n and NA
-    if (which(row == "y") ==0){ 
-      single_value <- "n"
-#       single_value <- NULL
-    } else {# y's are present
-      yindex <- which(row == "y")
-      row <- NULL # reset row to clean
-      for(j in 1:length(yindex)){ # go through yindex
-        row <- c(row, colnames(treatment_table)[j])
-        single_value <- paste(as.character(row), collapse= "/") # example: produces pltx/neo
+    # if they are all NA, put NA, if all n, put n, if all y, then list all of them
+    if (all(IndexedRow == IndexedRow[1])){
+      if ( IndexedRow[1] == "n"){ #all "n"
+        single_value <- IndexedRow[1]
+      } 
+      else if (IndexedRow[1] == "na"){ #all NA
+        single_value <- NA
+      }
+      else {# all are y!
+        single_value <- paste(as.character(colnames(treatment_table)), collapse = "/")
+      }
+      
+      ### if entries are not all the same, take y's  
+    } else {
+      #refresh single_value
+      single_value <- NULL 
+      # if none of the elements are y, then mix of n and NA
+      if (all(which(row == "y")) ==0){ 
+        single_value <- "n"
+        #       single_value <- NULL
+      } else {# y's are present
+        yindex <- which(row == "y")
+        row <- NULL # reset row to clean
+        for(j in 1:length(yindex)){ # go through yindex
+          row <- c(row, colnames(treatment_table)[j])
+          single_value <- paste(as.character(row), collapse= "/") # example: produces pltx/neo
+        }
       }
     }
+    treatment_values <- c(treatment_values, single_value)
   }
-treatment_values <- c(treatment_values, single_value)
+  
+  #### adding treatment column
+  PHENOdata <- cbind(PHENOdata, treatment_values)
+  
+  # phenodata and featuredata in AnnotatedDataFrame
+  PData <- AnnotatedDataFrame(data = PHENOdata)
+  Exprs <- exprs(currenteset)
+  
+  ################# Rename pData to standard names
+  colnames(PData) <- c("samplename", "tissue", "grade", "age", "t.rfs", "t.os", "e.os", "e.rfs", "series", "dataset", "treatment")
+  
+  
+  ################# make the expression set with exprs and PData
+  neweset <- list()
+  neweset[1]<- ExpressionSet(Exprs, phenoData =PData, experimentData=experimentData(currenteset), featureData = featureData(currenteset),  protocolData= protocolData(currenteset), annotation=annotation(currenteset))
+  
+  OvarianEsets[i] <- neweset
 }
-
-#### adding treatment column
-
-PHENOdata <- cbind(PHENOdata, treatment_values)
-
-# assayData matrix
-exprs <- matrix(0, nrow(fData(esets[[1]])), ncol = nrow(PHENOdata))
-
-
-# phenodata and featuredata in AnnotatedDataFrame
-PData <- AnnotatedDataFrame(data = PHENOdata)
-FData <- AnnotatedDataFrame(data = fData(esets[[1]]))
-
-################# Rename pData to standard names
-colnames(PData) <- c("tissue", "grade", "age", "t.trs", "t.os", "e.os", "e.rfs", "series", "dataset", "treatment")
-
-
-################# make the expression set with exprs and PData
-neweset<- ExpressionSet(exprs, phenoData =PData, experimentData=experimentData(esets[[1]]), featureData = featureData(esets[[1]]),  protocolData= protocolData(esets[[1]]), annotation=annotation(esets[[1]]))
