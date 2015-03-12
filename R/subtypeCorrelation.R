38e074eab8a5c5132f4f1227e0ac67256552bb14
Natchar <Natchar.r@alumni.ubc.ca>
2015-03-12 11:41:38 -0400

    label option for symbol or entrez
    added option label for gene "symbol" or "entrez"

--- R/subtypeCorrelation.R
+++ R/subtypeCorrelation.R
@@ -6,12 +6,12 @@
 
 ########################
 ## Natchar Ratanasirigulchai
-## changes made on March 2, 2015
+## Changes Made on: March 12, 2015
 ########################
 
 
 `subtypeCorrelation` <- 
-function (eset, sig, method=c("pearson", "spearman", "kendall"), weighted=FALSE, condensed=TRUE, plot=TRUE, resdir="cache", nthread=1, sig.method, sig.scaling) {
+function (eset, sig, method=c("pearson", "spearman", "kendall"), weighted=FALSE, condensed=TRUE, plot=TRUE, resdir="cache", nthread=1, sig.method, sig.scaling, label=c("symbol", "entrez")) {
   ## assess (weighted) correlation between gene expression with respect to subtypes
   #
   # Arga:
@@ -19,6 +19,7 @@ function (eset, sig, method=c("pearson", "spearman", "kendall"), weighted=FALSE,
   #   geneid: vector of Entrez Gene IDs. If missing, all genes will be considered.
   #   plot: should the correlation heatmap be plotted?
   #   method: method for correlation
+  #   label: labels by symbol or entrezgene ID
   #   resdir
   #   nthread:
   #   ...: parameters to be passed to sigScore
@@ -46,6 +47,8 @@ function (eset, sig, method=c("pearson", "spearman", "kendall"), weighted=FALSE,
   if (is.null(names(sig))) {
     names(sig) <- paste("SIG", 1:length(sig), sep=".")
   }
+  
+  label <- match.arg(label)
   
   if (!file.exists(file.path(resdir))) { dir.create(file.path(resdir), showWarnings=FALSE, recursive=TRUE) }
   
@@ -78,8 +81,14 @@ function (eset, sig, method=c("pearson", "spearman", "kendall"), weighted=FALSE,
     return (res)
   }, sig=sig, eset=eset, sigm=sig.method, sigs=sig.scaling)
   expr <- t(do.call(cbind, do.call(c, mcres)))
+  
+ if(label == "symbol"){
+   rownames(expr) <- fData(eset)[names(sig),"SYMBOL"]
+ } else if(label == "entrez"){
+   rownames(expr) <- names(sig)
+ } else{
   rownames(expr) <- names(sig)
-    
+ }  
     
   ## compute subtype-specific pairwise correlation across query genes
   
@@ -119,14 +128,32 @@ function (eset, sig, method=c("pearson", "spearman", "kendall"), weighted=FALSE,
   mRMRe::set.thread.count(nn)
   
   if (plot) {
+    
+    # Function to plot color bar
+    color.bar <- function(lut, min, max=-min, nticks=11, ticks=seq(min, max, len=nticks), title='') {
+      scale = (length(lut)-1)/(max-min)
+      
+#       dev.new(width=1.75, height=5)
+      plot(c(0,10), c(min,max), type='n', bty='n', xaxt='n', xlab='', yaxt='n', ylab='correlation', main=title)
+      axis(2, ticks, las=1)
+      for (i in 1:(length(lut)-1)) {
+        y = (i-1)/scale + min
+        rect(0,y,10,y+1/scale, col=lut[i], border=NA)
+      }	
+    }
+    
     if (condensed) {
-      nc <- 3
+      nc <- 4
       nr <- ceiling(length(rr) / nc)
+      pdf(file.path(resdir, "legend.pdf"), width= 1.75, height=5)
+      color.bar(colorRampPalette(c("blue","white","red"))(256), -1)
+      
+      dev.off()
       pdf(file.path(resdir, "subtype_correlation.pdf"), width=nc * 5, height=nr * 5)
       par(mfrow=c(nr, nc), cex=0.8, mar=c(7, 7, 3, 1) + 0.1, xaxt="n", yaxt="n")
       for (i in 1:length(rr)) {
         xx <- rr[[i]]
-        image(z=xx, zlim=c(-1, 1), col=blueYellow(256))
+        image(z=xx, zlim=c(-1, 1), col=colorRampPalette(c("blue","white","red"))(256))
         # axis(1, at=seq(1, nrow(xx), by=1), labels=FALSE)
         text(x=seq(par("usr")[1] + par("usr")[2] * 0.075, par("usr")[2] - par("usr")[2] * 0.025, length.out=nrow(xx)), y=par("usr")[3] - (par("usr")[4] * 0.025), pos=2, labels=rownames(xx), srt=45, xpd=NA, cex=0.8, font=2)
         text(x=par("usr")[1], y=seq(par("usr")[3] + par("usr")[4] * 0.05, par("usr")[4] - par("usr")[4] * 0.05, length.out=nrow(xx)), pos=2, labels=rownames(xx), srt=0, xpd=NA, cex=0.8, font=2)
@@ -137,6 +164,7 @@ function (eset, sig, method=c("pearson", "spearman", "kendall"), weighted=FALSE,
         }
         title(main=ss)
       }
+      
       dev.off()
     } else {
       for (i in 1:length(rr)) {
@@ -153,6 +181,7 @@ function (eset, sig, method=c("pearson", "spearman", "kendall"), weighted=FALSE,
           ss <- sprintf("Co-expression for %s subtype", names(rr)[i])
         }
         title(main=ss)
+        color.bar(colorRampPalette(c("blue", "yellow"))(256), -1)
         dev.off()
       }
     }
