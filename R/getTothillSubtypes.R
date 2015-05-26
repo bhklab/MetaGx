51970440886f7e875348206e3ad46eb04c762337
gmchen <gregorymchen@gmail.com>
2015-05-26 17:04:40 -0400

    Added code for structuring data for Tothill subtyping scheme

--- R/getTothillSubtypes.R
+++ R/getTothillSubtypes.R
@@ -1,17 +1,43 @@
-getTothillSubtypes <- function(eset, probe.mapping=c("probesets","entrez.ids")) {
-  probe.mapping <- match.arg(probe.mapping)
+getTothillSubtypes <- function(eset) {
+  ## Load train data with predefined class labels
+  #supplementary.data <- scan(system.file(file.path("extdata", "tothill.supptable.2.probesets.txt"), package="MetaGx"))
+  supplementary.probesets <- scan("../inst/extdata/tothill.supptable.2.probesets.txt", what=character(0), quiet=TRUE)
   
-	## Load train data with predefined class labels
-  #supplementary.data <- read.table(system.file(file.path("extdata", "../inst/extdata/tothill.supptable.2.txt"), package="MetaGx"), header=TRUE)
-  supplementary.data <- read.table("../inst/extdata/tothill.supptable.2.txt", header=TRUE)
+  # Get the probeset - Entrez ID mapping for the platform used in the Tothill et al. study
+  probe.entrez.mappings <- as.list(hgu133plus2ENTREZID[mappedkeys(hgu133plus2ENTREZID)])
+  
+  supplementary.entrez.ids <- unlist(probe.entrez.mappings[supplementary.probesets])
+  
   ## Train a diagonal linear discriminant classifier using the Tothill data set and overlapping probesets / entrez gene ids
-  tothill.eset <- esets$GSE9891
+  # Get the expression matrix of this eset and the Tothill eset, with columns named by Entrez gene ids
+  expression.matrix <- exprs(eset)
+  rownames(expression.matrix) <- fData(eset)$EntrezGene.ID[match(rownames(expression.matrix), rownames(fData(eset)))]
+ 
+  tothill.eset <- getGeneMapping(esets$GSE9891)
+  tothill.expression.matrix <- exprs(tothill.eset)
+  rownames(tothill.expression.matrix) <- fData(tothill.eset)$EntrezGene.ID[match(rownames(tothill.expression.matrix), rownames(fData(tothill.eset)))]
   
-  if(probe.mapping == "probesets") {
-    intersecting.probesets <- intersect(rownames(exprs(tothill.eset)), supplementary.data$probeset)
-  }
-  else {
-    ## TODO
-  }
+  
+  colnames(tothill.expression.matrix) <- sub("X", "", pData(tothill.eset)$alt_sample_name)
+  
+  intersecting.entrez.ids <- intersect(supplementary.entrez.ids, intersect(rownames(expression.matrix), rownames(tothill.expression.matrix)))
+  expression.matrix <- expression.matrix[rownames(expression.matrix) %in%  intersecting.entrez.ids,]
+  tothill.expression.matrix <- tothill.expression.matrix[rownames(tothill.expression.matrix) %in%  intersecting.entrez.ids,]
+  
+  #Transpose matrices, so each row is a sample and columns are genes  
+  expression.matrix <- t(expression.matrix)
+  tothill.expression.matrix <- t(tothill.expression.matrix)
+  
 	## Train and classify with diagonal LDA
+  #supplementary.classes <- read.table(system.file(file.path("extdata", "tothill.supptable.1.classes.txt"), package="MetaGx"), header=TRUE)
+  supplementary.classes <- read.table("../inst/extdata/tothill.supptable.1.classes.txt", header=TRUE)
+  supplementary.classes <- supplementary.classes[supplementary.classes$group != "NC",]
+  supplementary.classes$group <- as.factor(supplementary.classes$group)
+  levels(supplementary.classes$group) <- paste0("C", levels(supplementary.classes$group))
+  rownames(supplementary.classes) <- supplementary.classes$ID
+  supplementary.classes <- supplementary.classes[,-1,drop=FALSE]
+  
+  tothill.train.data <- merge(supplementary.classes, tothill.expression.matrix, by="row.names")
+  trained.dlda <- dlda(group ~ ., data=tothill.train.data)
+
 }
