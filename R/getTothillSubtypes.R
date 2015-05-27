3dc39ddc1b1c7fec680c14ac00ac9602bb1bdf67
gmchen <gregorymchen@gmail.com>
2015-05-27 13:32:46 -0400

    Completed Tothill subtyping scheme with Entrez IDs

--- R/getTothillSubtypes.R
+++ R/getTothillSubtypes.R
@@ -1,12 +1,14 @@
 getTothillSubtypes <- function(eset) {
   ## Load train data with predefined class labels
   #supplementary.data <- scan(system.file(file.path("extdata", "tothill.supptable.2.probesets.txt"), package="MetaGx"))
-  supplementary.probesets <- scan("../inst/extdata/tothill.supptable.2.probesets.txt", what=character(0), quiet=TRUE)
+  #supplementary.probesets <- scan("../inst/extdata/tothill.supptable.2.probesets.txt", what=character(0), quiet=TRUE)
   
   # Get the probeset - Entrez ID mapping for the platform used in the Tothill et al. study
-  probe.entrez.mappings <- as.list(hgu133plus2ENTREZID[mappedkeys(hgu133plus2ENTREZID)])
+  #probe.entrez.mappings <- as.list(hgu133plus2ENTREZID[mappedkeys(hgu133plus2ENTREZID)])
+  #supplementary.entrez.ids <- unlist(probe.entrez.mappings[supplementary.probesets])
   
-  supplementary.entrez.ids <- unlist(probe.entrez.mappings[supplementary.probesets])
+  supp.table.2 <- read.table("../inst/extdata/tothill.supptable.probes.entrez.txt", header=TRUE)
+  supplementary.entrez.ids <- unique(supp.table.2[supp.table.2$Entrez.ID != "---",]$Entrez.ID)
   
   ## Train a diagonal linear discriminant classifier using the Tothill data set and overlapping probesets / entrez gene ids
   # Get the expression matrix of this eset and the Tothill eset, with columns named by Entrez gene ids
@@ -28,9 +30,10 @@ getTothillSubtypes <- function(eset) {
   expression.matrix <- t(expression.matrix)
   tothill.expression.matrix <- t(tothill.expression.matrix)
   
-	## Train and classify with diagonal LDA
+	## Train and classify with diagonal LDA based on the cla
   #supplementary.classes <- read.table(system.file(file.path("extdata", "tothill.supptable.1.classes.txt"), package="MetaGx"), header=TRUE)
   supplementary.classes <- read.table("../inst/extdata/tothill.supptable.1.classes.txt", header=TRUE)
+  supplementary.classes$group <- as.character(supplementary.classes$group)
   supplementary.classes <- supplementary.classes[supplementary.classes$group != "NC",]
   supplementary.classes$group <- as.factor(supplementary.classes$group)
   levels(supplementary.classes$group) <- paste0("C", levels(supplementary.classes$group))
@@ -38,6 +41,12 @@ getTothillSubtypes <- function(eset) {
   supplementary.classes <- supplementary.classes[,-1,drop=FALSE]
   
   tothill.train.data <- merge(supplementary.classes, tothill.expression.matrix, by="row.names")
-  trained.dlda <- dlda(group ~ ., data=tothill.train.data)
-
+  rownames(tothill.train.data) <- tothill.train.data$Row.names
+  tothill.train.data <- tothill.train.data[,-1]
+  print("Training a DLDA classifier based on common genes...")
+  trained.dlda <- HiDimDA::Dlda(data=tothill.train.data[,-1], grouping=tothill.train.data$group)
+  print("Finished training.")
+  subclasses <- predict(trained.dlda, expression.matrix, grpcodes=levels(tothill.train.data$group))$class
+  pData(eset) <- data.frame(pData(eset), Tothill.subtypes=subclasses)
+  return(list(Annotated.eset=eset, trained.dlda=trained.dlda))
 }
