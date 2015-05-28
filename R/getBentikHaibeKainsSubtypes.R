93abc3f604f158eca9c23cefc00c69b502faabc7
gmchen <gregorymchen@gmail.com>
2015-05-28 11:15:44 -0400

    Fixed parameter in Bentink subtypes

--- R/getBentikHaibeKainsSubtypes.R
+++ R/getBentikHaibeKainsSubtypes.R
@@ -1,10 +1,9 @@
 getBentinkHaibeKainsSubtypes <- function(eset) {
-	## Use predefined class labels to train diagonal LDA
 	## Classify new samples
   expression.matrix <- t(exprs(eset))
   annot <- fData(eset)
   colnames(annot)[which(colnames(annot) == "EntrezGene.ID")] <- "entrezgene"
-  angio <- ovcAngiogenic(data = data, annot=annot, gmap="entrezgene", do.mapping = TRUE)
-  pData(eset) <- data.frame(pData(eset), Bentink.Haibe.Kains.subtypes=angio$subtype)
+  angio <- ovcAngiogenic(data = expression.matrix, annot=annot, gmap="entrezgene", do.mapping = TRUE)
+  pData(eset) <- data.frame(pData(eset), Bentink.Haibe.Kains.subtypes=angio$subtype$subtype)
   return(list(Annotated.eset=eset, angio=angio))
 }
