8defb9affcfe68b4243d34076c17524772705825
gmchen <gregorymchen@gmail.com>
2015-05-29 13:23:16 -0400

    Added relevant images, more vignette edits

--- R/getBentinkHaibeKainsSubtypes.R
+++ R/getBentinkHaibeKainsSubtypes.R
@@ -3,7 +3,7 @@ getBentinkHaibeKainsSubtypes <- function(eset) {
   expression.matrix <- t(exprs(eset))
   annot <- fData(eset)
   colnames(annot)[which(colnames(annot) == "EntrezGene.ID")] <- "entrezgene"
-  angio <- ovcAngiogenic(data = expression.matrix, annot=annot, gmap="entrezgene", do.mapping = TRUE)
+  angio <- genefu::ovcAngiogenic(data = expression.matrix, annot=annot, gmap="entrezgene", do.mapping = TRUE)
   pData(eset) <- data.frame(pData(eset), Bentink.Haibe.Kains.subtypes=angio$subtype$subtype)
   return(list(Annotated.eset=eset, angio=angio))
 }
