5cf9d3bf54b6a690494671f12e15eac67c65a693
gmchen <gregorymchen@gmail.com>
2015-06-03 17:25:46 -0400

    Implemented the two-stage Verhaak classification procedure

--- R/getVerhaakSubtypes.R
+++ R/getVerhaakSubtypes.R
@@ -3,22 +3,49 @@ getVerhaakSubtypes <- function(eset) {
   # Load Verhaak et al. supplementary from the package inst directory
 	#supplementary.data <- read.xls(system.file(file.path("extdata", "JCI65833sd1.xls"), package="MetaGx"), sheet=7, skip=1)
   # Use this instead when running this method from source
-	supplementary.data <- read.xls("../inst/extdata/JCI65833sd1.xls", sheet=7, skip=1)
-
-	genesets <- lapply(levels(supplementary.data$CLASS), function(y) as.character(supplementary.data[supplementary.data$CLASS==y,1]))
-	names(genesets) <-  levels(supplementary.data$CLASS)
+	supplementary.data.sheet7 <- read.xls("../inst/extdata/JCI65833sd1.xls", sheet=7, skip=1)
+	supplementary.data.sheet1 <- read.xls("../inst/extdata/JCI65833sd1.xls", skip=1)
   
+	genesets <- lapply(levels(supplementary.data.sheet7$CLASS), function(y) as.character(supplementary.data.sheet7[supplementary.data.sheet7$CLASS==y,1]))
+	names(genesets) <-  levels(supplementary.data.sheet7$CLASS)
+	
+	# For ssGSEA scores for the new samples, use the intersecting genes
+	genesets <- lapply(genesets, function(x) intersect(x, fData(eset)$gene) )
+  
+  ## Determine the ssGSEA cutoffs for the IMR and MES subtypes
+	supplementary.tcga.discovery <- supplementary.data.sheet1[ supplementary.data.sheet1$DATASET=="TCGA-discovery", 
+                                                             c("ID", "SUBTYPE") ]
+	supplementary.tcga.discovery <- supplementary.tcga.discovery[ supplementary.tcga.discovery$SUBTYPE %in% c("Mesenchymal", "Immunoreactive"), ]
+  
+  tcga.eset <- esets$TCGA
+  tcga.eset <- tcga.eset[,tcga.eset$unique_patient_ID %in% supplementary.tcga.discovery$ID]
+  tcga.expression.matrix <- exprs(tcga.eset)
+  rownames(tcga.expression.matrix) <- fData(tcga.eset)$gene
+  tcga.gsva.out <- gsva(tcga.expression.matrix, genesets, method="ssgsea", min.sz=10, tau=0.75, parallel.sz=4)
+  tcga.gsva.out <- as.data.frame(t(tcga.gsva.out))
+  tcga.gsva.out$ID = tcga.eset$unique_patient_ID
+	
+  tcga.gsva.out.with.published.subtype <- merge(tcga.gsva.out, supplementary.tcga.discovery, by="ID")
+  IMR.threshold <- min(tcga.gsva.out.with.published.subtype$IMR[ tcga.gsva.out.with.published.subtype$SUBTYPE=="Immunoreactive" ])
+  MES.threshold <- min(tcga.gsva.out.with.published.subtype$MES[ tcga.gsva.out.with.published.subtype$SUBTYPE=="Mesenchymal" ])
+ 
 	expression.matrix <- exprs(eset)
-  rownames(expression.matrix) <- fData(eset)$gene[match(rownames(expression.matrix), rownames(fData(eset)))]
+  rownames(expression.matrix) <- fData(eset)$gene
   
   ## Get ssGSEA subtype scores
 	gsva.out <- gsva(expression.matrix, genesets, method="ssgsea", min.sz=10, tau=0.75, parallel.sz=4)
   gsva.out <- t(gsva.out)
-  ## Classify each sample according to the max ssGSEA subtype score. Note that this differs slightly
-  # from the Veerhak et al. classification which has a "first pass" for classifying
-  # Immunoreactive and Mesenchymal classes, followed by classification of remaining samples into
-  # the max-scoring ssGSEA value for the four subtypes.
-  subclasses <- as.factor(apply(gsva.out, 1, function(x) colnames(gsva.out)[which.max(x)]))
+    
+  ## Classify each sample according to the max ssGSEA subtype score, using the scheme provided in the methods.
+  
+  subclasses <- apply(gsva.out, 1, function(x) {
+    if(x[which(colnames(gsva.out)=="IMR")] > IMR.threshold && x[which(colnames(gsva.out)=="MES")] > MES.threshold) {
+      return(c("IMR", "MES")[which.max(x[c("IMR", "MES")])])
+      }
+    colnames(gsva.out)[which.max(x)]
+    })
+  
+  subclasses <- as.factor(subclasses)
   ## Append a new column for Verhaak subtypes
   eset$Verhaak.subtypes <- subclasses
   return(list(Annotated.eset=eset, gsva.out=gsva.out))
