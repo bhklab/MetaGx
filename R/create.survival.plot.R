fe10644ca7528758a57c88043e5e18a060f01760
gmchen <gregorymchen@gmail.com>
2015-06-10 18:03:25 -0400

    Added CD73 case study

--- R/create.survival.plot.R
+++ R/create.survival.plot.R
@@ -1,6 +1,9 @@
 
 create.survival.plot <- function(
-                                 surv.obj,
+                                 surv.time,
+                                 surv.event,
+                                 groups,
+                                 risk.order.same.as.groups = TRUE, # for computing c-index and d-index, should the risk order be the same as the ordered factor 
                                  xlab="Time",
                                  ylab="Survival",
                                  main="Survival Plot",
@@ -11,12 +14,28 @@ create.survival.plot <- function(
                                  reverse.legend.order=FALSE,
                                  legend.pos="topright",
                                  legend.lwd=5,
+                                 stats.to.show=c("n","p","d","c"), # an ordered vector of stats to show: n for the number of samples, p for p-value of the likelihood-ratio test, d for d-index, c for c-index
                                  # Most relevant legend parameters are already covered, but allow further parameters
                                  legend.par=list(),
                                  ...) {
-  
-  if(is.null(strata.names)) {
+  surv.obj <- survfit(Surv(surv.time, surv.event) ~ groups)
+ 
+   if(is.null(strata.names)) {
     strata.names <- names(surv.obj$strata)
+  }
+  
+  if(!is.null(stats.to.show) && !all(stats.to.show %in% c("n","p","d","c"))) {
+    stop("Argument stats.to.show should only contain 'n', 'p', 'd', 'c'.")
+  }
+  if(any(c("c", "d") %in% stats.to.show)) {
+    if(!is.ordered(groups)) {
+      stop("For calculation of c-index and d-index, groups must be an ordered factor")
+    }
+    if(risk.order.same.as.groups == TRUE) {
+      risk.prediction <- as.numeric(groups)
+    } else {
+      risk.prediction <- -as.numeric(groups)
+    }
   }
   
   # In all cases, make sure that the associations between colours, legend labels, and survival curves are consistent
@@ -33,7 +52,7 @@ create.survival.plot <- function(
   }
   
   plot(
-    ctla4.surv.obj,
+    surv.obj,
     xlab=xlab,
     ylab=ylab,
     main=main,
@@ -60,4 +79,30 @@ create.survival.plot <- function(
   }
   
   do.call(legend, legend.par)
+  
+  # display stats
+  coxph.summary <- summary(survival::coxph(Surv(surv.time, surv.event) ~ groups))
+  n <- coxph.summary$n
+  p <- coxph.summary$logtest[["pvalue"]]
+  c <- survcomp::concordance.index(risk.prediction, surv.time, surv.event)$c.index
+  d <- survcomp::D.index(risk.prediction, surv.time, surv.event)$d.index
+  
+  if(length(stats.to.show) > 0) {
+    text.to.show <- ""
+    for(i in 1:length(stats.to.show)) {
+      if(i != 1) {
+          text.to.show <- paste0(text.to.show, "\n")
+      }
+      if(stats.to.show[i] == "n") {
+        text.to.show <- paste0(text.to.show, "n = ", n)
+      } else if(stats.to.show[i] == "p") {
+        text.to.show <- paste0(text.to.show, "p = ", round(p, digits=3))
+      } else if(stats.to.show[i] == "c") {
+        text.to.show <- paste0(text.to.show, "c-index: ", round(c, digits=3))
+      } else if(stats.to.show[i] == "d") {
+        text.to.show <- paste0(text.to.show, "d-index: ", round(d, digits=3))
+      } 
+    }
+    invisible(plotrix::corner.label(text.to.show, x=-1, y=-1))
+  }
 }
