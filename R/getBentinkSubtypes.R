950bbd4fa5231f984dcd15a5370b53e0af87fe60
mzon7 <michaelzon7@gmail.com>
2017-08-16 18:23:49 -0400

    Remove duplicated directory

--- R/getBentinkSubtypes.R
+++
@@ -1,9 +0,0 @@
-getBentinkSubtypes <- function(eset) {
-	## Classify new samples
-  expression.matrix <- t(exprs(eset))
-  annot <- fData(eset)
-  colnames(annot)[which(colnames(annot) == "EntrezGene.ID")] <- "entrezgene"
-  angio <- genefu::ovcAngiogenic(data = expression.matrix, annot=annot, gmap="entrezgene", do.mapping = TRUE)
-  eset$Bentink.subtypes <- angio$subtype$subtype
-  return(list(Annotated.eset=eset, angio=angio))
-}
